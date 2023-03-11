cf8581afabd90259705290d221cd1d84806591a0
alenca <nicosavioparedes@gmail.com>
2023-03-11 14:44:49 -0300

    Delete src directory

--- src/config.js
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-
-config();
-
-export const MDB_URI =
-  process.env.MDB_URI ||
-  "mongodb+srv://alenca:<password>@cluster0.rbjmmvp.mongodb.net/?retryWrites=true&w=majority";
